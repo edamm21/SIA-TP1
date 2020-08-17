2d719802166f688d2f839bab073ce7435a3007bf
EugeDamm <euge.damm@gmail.com>
2020-08-17 11:41:58 -0300

    added sokoban website assets for javafx, added scene creation and result analytics

--- src/main/java/Graphics.java
+++ src/main/java/Graphics.java
@@ -1,100 +1,114 @@
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import javafx.scene.control.Button;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Graphics extends Application {
 
-    public static final int TILE_SIZE = 100;
-    private Group tileGroup = new Group();
-    private Group pieceGroup = new Group();
-    private Board initialBoard;
+    private Stack<Node> path = new Stack<>();
+    private List<Scene> scenes = new ArrayList<>();
+    private int finalDepth;
 
-    private Parent createContent() {
-        Pane root = new Pane();
-        int width = initialBoard.getWidth();
-        int height = initialBoard.getHeight();
-        root.setPrefSize(width * TILE_SIZE, height * TILE_SIZE);
-        root.getChildren().addAll(tileGroup, pieceGroup);
-        for(int y = 0 ; y < height ; y++) {
-            for(int x = 0 ; x < width ; x++){
-                Tile tile;
-                switch (initialBoard.getBoard()[y][x]) {
-                    case '#':
-                        tile = new Tile(Color.BROWN, x, y);
-                        break;
-                    case '@':
-                        tile = new Tile(Color.BLUE, x, y);
-                        break;
-                    case '$':
-                        tile = new Tile(Color.RED, x, y);
-                        break;
-                    case '.':
-                        tile = new Tile(Color.YELLOW, x, y);
-                        break;
-                    default:
-                        tile = new Tile(Color.BLACK, x, y);
-                        break;
+    private Scene createNextScene(Node node, Stage stage, int move) {
+        if(node == null)
+            return new Scene(new Pane());
+        int width = ArtificialIntelligence.initialBoard.getWidth();
+        int height = ArtificialIntelligence.initialBoard.getHeight();
+        Pane pane = new Pane();
+        if(!path.isEmpty()) {
+            final String analyticsText = "Current Move Analytics:\nCurrent depth:" + move ;
+            Button nextButton = new Button("Move " + (move + 1));
+            nextButton.setOnAction(e -> stage.setScene(createNextScene(path.pop(), stage, move + 1)));
+            Button prevButton = null;
+            TextArea textArea = new TextArea();
+            textArea.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 10));
+            textArea.setPrefSize(4 * ArtificialIntelligence.TILE_SIZE, height * ArtificialIntelligence.TILE_SIZE);
+            textArea.setPrefColumnCount(1);
+            textArea.setText(analyticsText);
+            textArea.setLayoutX(width * ArtificialIntelligence.TILE_SIZE);
+            pane.setPrefSize(width * ArtificialIntelligence.TILE_SIZE + 3 * ArtificialIntelligence.TILE_SIZE, height * ArtificialIntelligence.TILE_SIZE);
+            pane.getChildren().addAll(ArtificialIntelligence.tileGroup, ArtificialIntelligence.pieceGroup, nextButton, textArea);
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    Tile tile;
+                    switch (node.getBoard().getBoard()[y][x]) {
+                        case '#':
+                            tile = new Tile("/assets/wall.png", x, y);
+                            break;
+                        case '@':
+                            tile = new Tile("/assets/player.png", x, y);
+                            break;
+                        case '$':
+                            tile = new Tile("/assets/box.png", x, y);
+                            break;
+                        case '+':
+                            tile = new Tile("/assets/player-on-goal.png", x, y);
+                            break;
+                        case '*':
+                            tile = new Tile("/assets/box-on-goal.png", x, y);
+                            break;
+                        case ' ':
+                            tile = new Tile("/assets/floor.png", x, y);
+                            break;
+                        case '.':
+                            tile = new Tile("/assets/goal.png", x, y);
+                            break;
+                        default:
+                            tile = new Tile("/assets/wall.png", x, y);
+                            break;
+                    }
+                    ArtificialIntelligence.tileGroup.getChildren().add(tile);
                 }
-                tileGroup.getChildren().add(tile);
             }
+            Scene scene = new Scene(pane);
+            scenes.add(scene);
+            return scene;
+        } else {
+            Pane paneFinal = new Pane();
+            paneFinal.setPrefSize(width * ArtificialIntelligence.TILE_SIZE + 3 * ArtificialIntelligence.TILE_SIZE, height * ArtificialIntelligence.TILE_SIZE);
+            Text text = new Text();
+            text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+            long elapsedTime = ArtificialIntelligence.elapsedTime;
+            text.setText("\nRESULTS:\n" +
+                    "Solution found: true\n" +
+                    "Time elapsed: " + (elapsedTime > 60000 ? (((elapsedTime / 1000.0) * 60) + " minutes.\n") : ((elapsedTime / 1000.0) + " seconds.\n")) +
+                    "Depth reached: " + finalDepth + "\n" +
+                    "Total nodes expanded: " + ArtificialIntelligence.nodesExpanded + "\n" +
+                    "Frontier nodes: " + ArtificialIntelligence.frontierNodes
+            );
+            paneFinal.getChildren().addAll(text);
+            return new Scene(paneFinal);
         }
-        return root;
     }
 
-    public void drawNewBoard(Board board) {
-
+    private Scene createAllScenes(Stage stage) {
+        Node n = ArtificialIntelligence.solution;
+        while(n.getParentNode() != null) {
+            path.push(n);
+            n = n.getParentNode();
+        }
+        path.push(n);
+        finalDepth = path.size();
+        return createNextScene(path.pop(), stage, 0);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        initialConfig();
-        Scene scene = new Scene(createContent());
+        Scene scene = createAllScenes(primaryStage);;
         primaryStage.setTitle("Sokoban Solver");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-    private void initialConfig() throws Exception{
-        String algorithm;
-        String heuristic;
-        try {
-            List<String> configurations = Files.readAllLines(Paths.get("settings.conf"));
-            algorithm = configurations.get(0);
-            heuristic = configurations.get(1);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-        System.out.println("Algorithm: " +algorithm +"\nHeuristic: " +heuristic);
-
-        // Read initial board file
-        String initBoard;
-        try {
-            initBoard = String.join("\n", Files.readAllLines(Paths.get("map.conf")));
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-        this.initialBoard = new Board(initBoard);
-        initialBoard.printBoard();
-
-        System.out.println("\nPlayer: " + initialBoard.getPlayerPosition());
-        System.out.println("Goals: " + initialBoard.getGoalPositions());
-        System.out.println("Boxes: " + initialBoard.getBoxPositions());
-        System.out.println("Finished: " + initialBoard.isCompleted());
-
-        //ArtificialIntelligence artificialIntelligence = new ArtificialIntelligence("DFS", 10000);
-        //artificialIntelligence.solve(b);
-
     }
 
     private Piece makePlayer(TileTypes tileType, int x, int y) {
--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -1,4 +1,6 @@
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
 public class Tile extends Rectangle {
@@ -17,10 +19,11 @@ public class Tile extends Rectangle {
         this.piece = piece;
     }
 
-    public Tile(Color color, int x, int y) {
-        setWidth(Graphics.TILE_SIZE);
-        setHeight(Graphics.TILE_SIZE);
-        relocate(x * Graphics.TILE_SIZE, y * Graphics.TILE_SIZE);
-        setFill(color);
+    public Tile(String imageURL, int x, int y) {
+        setWidth(ArtificialIntelligence.TILE_SIZE);
+        setHeight(ArtificialIntelligence.TILE_SIZE);
+        relocate(x * ArtificialIntelligence.TILE_SIZE, y * ArtificialIntelligence.TILE_SIZE);
+        Image img = new Image(imageURL);
+        setFill(new ImagePattern(img));
     }
 }
